fd0f9ca75f92d9edcf6a97a6bf3e647db331b2f2
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-13 02:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-const team[];
-int(let i=0;i<players.length;i++){
+const team=[];
+for(let i=0;i<players.length;i++){
 	team[i]=players[i];
 }
-const team1[];
-int(let i=0;i<players.length;i++){
-	team1[i]=players[i];
+const team1=[];
+for(let x=0;x<players.length;x++){
+	team1[x]=players[x];
 }
 const cap1= person;
+// console.log(team);
+// console.log(team1);
+// console.log(cap1);
+// const cap1= person;
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
